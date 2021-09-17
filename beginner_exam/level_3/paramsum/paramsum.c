43c6676ec6625a994f909160c75caae225f1ed39
Srdan <srdan.vorkapic@outlook.com>
2021-09-17 19:45:57 +0200

    paramsum

---
+++ beginner_exam/level_3/paramsum/paramsum.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: srvorkap <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/14 08:20:24 by srvorkap          #+#    #+#             */
+/*   Updated: 2019/10/14 08:28:13 by srvorkap         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+int		main(int ac, char **av)
+{
+	int		arguments;
+
+	if (ac < 2 && !av)
+		write(1, "0\n", 2);
+	else
+	{
+		arguments = ac - 1;
+		ft_putnbr(arguments);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
